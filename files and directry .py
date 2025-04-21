3e41f2a18f10cd50f8b079e9025b57f5b71ca434
eslam <eslambkheet007@gmail.com>
2025-04-21 12:49:12 +0200

    new

--- files and directry .py
+++
@@ -1,7 +0,0 @@
-# list all files end with .txt in download folder 
-import os
-from pathlib import Path
-downloads = Path.home() / "Downloads"
-for files in downloads.iterdir():
-    if files.is_file() and files.suffix == ".txt":
-        print(files.name)
